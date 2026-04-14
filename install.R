fa16cc7009212be3422345706175e98075c6b87e
aijordan <aijordan@users.noreply.github.com>
2026-04-14 15:39:12 +0200

    setup for mybinder.org

---
+++ install.R
@@ -0,0 +1,3 @@
+install.packages(c('sp', 'fractaldim'))
+install.packages('https://cran.r-project.org/src/contrib/Archive/RandomFieldsUtils/RandomFieldsUtils_1.2.5.tar.gz')
+install.packages('https://cran.r-project.org/src/contrib/Archive/RandomFields/RandomFields_3.3.14.tar.gz')
